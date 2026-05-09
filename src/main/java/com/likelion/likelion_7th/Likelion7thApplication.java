301084674e7a97fa67e9ab8772f475d28f1b3bab
nahyun <nahyun040411@naver.com>
2026-05-09 22:13:49 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/likelion/likelion_7th/Likelion7thApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion_7th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion7thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion7thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/likelion_7th/Likelion7thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion_7th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion7thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
