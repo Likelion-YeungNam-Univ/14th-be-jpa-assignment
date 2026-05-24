075207a92a206b80d21419260e243905a17cedba
ryudayeong <rdy1125@yu.ac.kr>
2026-05-24 18:38:14 +0900

    feat: 도메인별 Repository 인터페이스 작성

---
+++ src/main/java/yu/likelion14th/_th/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package yu.likelion14th._th.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import yu.likelion14th._th.domain.User;
+
+// 회원 레포지토리
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUseremail(String useremail);
+}
